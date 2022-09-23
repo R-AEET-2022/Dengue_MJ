99c7ac4f67ed7d46228a87d87dbf5029637d2386
Maddi19 <maddiart19@gmail.com>
2022-09-23 11:29:35 +0200

    actualizacion script

--- scripts/script_gráficos.R
+++ scripts/script_gráficos.R
@@ -15,6 +15,7 @@ library(summarytools) #resume de forma clara y rápida datos numéricos y categ�
 library(knitr) #reportar datos en varios formatos
 library(magrittr) #poder encadenar funciones con "data %>% function"
 library(summarytools)
+library(effects)
 
 denge1 <- read_csv(here("data/dengue_features_train.csv"))
 denge2 <- read_csv(here("data/dengue_labels_train.csv"))
@@ -34,60 +35,107 @@ denge <- denge %>% select("city", "year", "weekofyear", "total_cases", "station_
 denge <- denge %>% rename(avg_temp = station_avg_temp_c, precip_mm = station_precip_mm, humidity = reanalysis_relative_humidity_percent)
 
 
-############# JAVI part
-dengue_year <- denge %>%
+deng <- denge %>%
   group_by(year, city) %>%
-  mutate(mean_tc_year = mean(total_cases)) %>%
+  mutate(cases = mean(total_cases)) %>%
   mutate(dias_muestreo= n())
 
-dengue_week <- dengue_year %>%
+dengue <- deng %>%
   group_by(weekofyear,city) %>%
-  mutate(mean_tc_week = mean(total_cases))
+  mutate(cases_estacionalidad=mean(total_cases))
+
+dengue$year<- as.character(dengue$year)
+dengue$weekofyear<- as.factor(dengue$weekofyear)
+denguesj <- subset(dengue, city=="sj")
+dengueiq <- subset(dengue, city=="iq")
+
+###pca
+library(vegan)
+den<- dengue[, 5:7]
+pca <- prcomp(den, center = TRUE,
+              scale = TRUE)
 
 
-dengue_year$year<- as.character(dengue_year$year)
+
+  library(ggbiplot)
+g <- ggbiplot(pca, obs.scale = 1, var.scale = 1,
+              groups = ir_species, ellipse = TRUE,
+              circle = TRUE)
+g <- g + scale_color_discrete(name = '')
+g <- g + theme(legend.direction = 'horizontal',
+               legend.position = 'top')
+print(g)
 
 #Temporales: 
+ggyear_mean <- ggplot(dengue, aes(x=year,
+                        y=cases,
+                        color=city))+
+  geom_point()+ geom_line()
 
-ggyear_mean <- ggplot(dengue_year, aes(x=year,
-                        y=mean_tc_year,
-                      color=city))+
-  geom_point() + geom_point()
-ggyear_mean
-
-ggweek_mean <- ggplot(dengue_week, aes(x = weekofyear,
-                        y = mean_tc_week,
+ggweek_mean <- ggplot(dengue, aes(x = weekofyear,
+                        y = cases_estacionalidad,
                         color= city)) +
   geom_point() + geom_smooth()
 
-ggweek_total <- ggplot(dengue_week, aes(x = weekofyear,
+ggweek_total <- ggplot(dengue, aes(x = weekofyear,
                         y = total_cases,
                         color= city)) +
   geom_point()+geom_smooth()
 
 #Climáticos: 
-ggtemp <- ggplot(denge) +
-  geom_point(aes(avg_temp, total_cases, color = city))
+ggtemp <- ggplot(dengue) +
+  geom_point(aes(avg_temp, total_cases, color = city))+
+  geom_smooth()
 
-ggprec <- ggplot(denge) +
-  geom_point(aes(precip_mm, total_cases, color = city))
+##escalar temperatura y precipitacion
+denguesj[,5:6] <- scale(denguesj[, 5:6], center = T, scale = T)
+dengueiq[,5:6] <- scale(dengueiq[, 5:6], center = T, scale = T)
+m0 <- 
 
-gghum <- ggplot(denge) +
-  geom_point(aes(humidity, total_cases, color = city))
+m4 <- glmer(total_cases~ avg_temp * precip_mm * humidity + (1|year), data=denguesj, family = poisson)
+summary(m4)
+check_model(m4)
+plot(allEffects(m4))
+simulateResiduals(m4, plot = T)
+library(visreg)
+visreg(m4)
+
+library(mgcv)
+denguesj$weekofyear<- as.numeric(denguesj$weekofyear)
+mod <- gam(total_cases ~ s(weekofyear), 
+           correlation = corCAR1(form = ~ weekofyear), data = denguesj,
+             family=poisson)
+summary(mod)
+check_model(mod)
+library(DHARMa)
+install.packages("mgcViz")
+library(mgcViz)
+simulateResiduals(mod, plot = T)
+visreg(mod)
+
+compare_performance(m4,mod)
+
+
+
+ggprec <- ggplot(dengue, aes(precip_mm, total_cases, color = city)) +
+  geom_point()
+
+gghum <- ggplot(dengue, aes(humidity, total_cases, color = city)) +
+  geom_point()
 
 #Mezcla: 
 
-ggtemp_time <- ggplot(dengue_year, aes(x = weekofyear,
+ggtemp_time <- ggplot(dengue, aes(x = weekofyear,
                         y = avg_temp,
                         color= city)) +
   geom_point(aes(size= total_cases)) + geom_smooth()
 
-ggprec_time <- ggplot(dengue_year, aes(x = weekofyear,
+ggprec_time <- ggplot(dengue, aes(x = weekofyear,
                         y = precip_mm,
                         color= city)) +
   geom_point(aes(size= total_cases)) + geom_smooth()
 
-gghum_time <- ggplot(dengue_year, aes(x = weekofyear,
+gghum_time <- ggplot(dengue, aes(x = weekofyear,
                         y = humidity,
                         color= city)) +
   geom_point(aes(size= total_cases)) + geom_smooth()
